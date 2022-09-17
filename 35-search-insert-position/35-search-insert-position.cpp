64ee830b5393a4a4bec91db37c34f10410054d0d
Aryan Kashyap <75727351+aryankashyap7@users.noreply.github.com>
2022-09-17 15:47:59 +0530

    Time: 20 ms (5.83%), Space: 9.7 MB (22.70%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int searchInsert(vector<int> &nums, int target)
+{
+
+    // Binary Search
+    int start = 0;
+    int end = nums.size() - 1;
+    int mid;
+
+    // Binary Search
+
+    while (start <= end)
+    {
+        mid = (start + end) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] > target)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+
+        // cout << "Start: " << start << " End: " << end << " Mid: " << mid << endl;
+    }
+
+    return (nums[mid] > target) ? mid : mid + 1;
+}
+};
