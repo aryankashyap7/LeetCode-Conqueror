525b899317a21d5ebcd9722c1c6d49a8e002ea25
Aryan Kashyap <75727351+aryankashyap7@users.noreply.github.com>
2022-11-13 23:19:05 +0530

    Time: 6 ms (84.31%), Space: 8.2 MB (44.99%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -27,3 +27,16 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         stringstream str(s);
+//         string word, ans = "";
+//         while(str >> word)
+//             ans = word + " " + ans;
+//         ans.pop_back();
+//         return ans;
+//     }
+// };
