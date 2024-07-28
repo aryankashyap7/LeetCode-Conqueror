6a115ea5af53ebc066a0c8e424c00fcd4893b201
Aryan Kashyap <75727351+aryankashyap7@users.noreply.github.com>
2024-07-28 15:04:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int reverse(int x) {
+        string s = to_string(x);
+        bool neg = false;
+
+        if (s[0] == '-') {
+            neg = true;
+            s = s.substr(1); // Correctly update s to exclude the negative sign
+        }
+
+        string reversedStr = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversedStr += s[i];
+        }
+
+        try {
+            int reversedInt = stoi(reversedStr);
+            return neg ? -reversedInt : reversedInt;}
+        catch(){
+            return 0;
+        }
+        // } catch (out_of_range &e) {
+        //     // If the reversed integer is out of range of a 32-bit signed integer
+        //     return 0;
+        // }
+    }
+};
